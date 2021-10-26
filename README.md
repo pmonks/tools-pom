d6107ccb5f9f5c5c90d0d0c04857a86eaea9574f
Peter Monks <pmonks@gmail.com>
2021-10-25 18:33:51 -0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+| | | |
+|---:|:---:|:---:|
+| [**main**](https://github.com/pmonks/tools-pom/tree/main) | [![Lint](https://github.com/pmonks/tools-pom/workflows/lint/badge.svg?branch=main)](https://github.com/pmonks/tools-pom/actions?query=workflow%3Alint) | [![Dependencies](https://github.com/pmonks/tools-pom/workflows/dependencies/badge.svg?branch=main)](https://github.com/pmonks/tools-pom/actions?query=workflow%3Adependencies) |
+| [**dev**](https://github.com/pmonks/tools-pom/tree/dev)  | [![Lint](https://github.com/pmonks/tools-pom/workflows/lint/badge.svg?branch=dev)](https://github.com/pmonks/tools-pom/actions?query=workflow%3Alint) | [![Dependencies](https://github.com/pmonks/tools-pom/workflows/dependencies/badge.svg?branch=dev)](https://github.com/pmonks/tools-pom/actions?query=workflow%3Adependencies) |
+
+[![Latest Version](https://img.shields.io/clojars/v/com.github.pmonks/tools-pom)](https://clojars.org/com.github.pmonks/tools-pom/) [![Open Issues](https://img.shields.io/github/issues/pmonks/tools-pom.svg)](https://github.com/pmonks/tools-pom/issues) [![License](https://img.shields.io/github/license/pmonks/tools-pom.svg)](https://github.com/pmonks/tools-pom/blob/main/LICENSE)
+
+
+<img alt="Ice cold can of hangover-inducing rubbish beer" align="right" width="25%" src="https://pabstblueribbon.com/wp-content/uploads/2020/10/pbr-org.png">
+
+# tools-pom
+
+Clojure tools.build task(s) related to the generation of comprehensive pom.xml files.
+
+## Features
+
+### API Documentation
+
+[API documentation is available here](https://pmonks.github.io/tools-pom/).
+
+### Build tasks
+
+1. `pom` - generate a comprehensive `pom.xml` file from EDN (which can come from anywhere - stored in your `deps.edn` or a separate file, or synthesised on the fly in your build tool script).
+
+Note that the `pom` task is entirely data-driven, so if your input data includes elements that are not valid in a [Maven POM](https://maven.apache.org/guides/introduction/introduction-to-the-pom.html), the resulting file will be invalid.  You can check your input data by enabling the `:validate-pom` flag in the options that get passed to the task.
+
+## Using the library
+
+### Dependency
+
+Express a maven dependency in your `deps.edn`, for a build tool alias:
+
+```edn
+  :aliases
+    :build
+      {:deps       {com.github.pmonks/tools-pom {:mvn/version "LATEST_CLOJARS_VERSION"}}
+       :ns-default your.build.ns}
+```
+
+### Adding comprehensive `pom` information and a `pom` build task to your build:
+
+```clojure
+(def lib       'com.github.yourusername/yourproject)
+(def version   (format "1.0.%s" (b/git-count-revs nil)))
+
+(defn- set-opts
+  [opts]
+  (assoc opts
+         :lib          lib
+         :version      version
+         :write-pom    true
+         :validate-pom true
+         ; Note: this EDN can come from anywhere - you could externalise it into a separate edn file (e.g. pom.edn), synthesise it from information elsewhere in your project, or whatever other scheme you like
+         :pom          {:description      "Description of your project e.g. your project's GitHub \"short description\"."
+                        :url              "https://github.com/yourusername/yourproject"
+                        :licenses         [:license   {:name "Apache License 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}] ; Note first element is a tag
+                        :developers       [:developer {:id "yourusername" :name "yourname" :email "youremail@emailservice.com"}]           ; And here
+                        :scm              {:url                  "https://github.com/yourusername/yourproject"
+                                           :connection           "scm:git:git://github.com/yourusername/yourproject.git"
+                                           :developer-connection "scm:git:ssh://git@github.com/yourusername/yourproject.git"}
+                        :issue-management {:system "github" :url "https://github.com/yourusername/yourproject/issues"}}))
+
+(defn pom
+  "Construct a comprehensive pom.xml file for this project"
+  [opts]
+  (-> opts
+      (set-opts)
+      (pom/pom)))
+```
+
+## Contributor Information
+
+[Contributing Guidelines](https://github.com/pmonks/tools-pom/blob/main/.github/CONTRIBUTING.md)
+
+[Bug Tracker](https://github.com/pmonks/tools-pom/issues)
+
+[Code of Conduct](https://github.com/pmonks/tools-pom/blob/main/.github/CODE_OF_CONDUCT.md)
+
+### Developer Workflow
+
+The repository has two permanent branches: `main` and `dev`.  **All development must occur either in branch `dev`, or (preferably) in feature branches off of `dev`.**  All PRs must also be submitted against `dev`; the `main` branch is **only** updated from `dev` via PRs created by the core development team.  All other changes submitted to `main` will be rejected.
+
+This model allows otherwise unrelated changes to be batched up in the `dev` branch, integration tested there, and then released en masse to the `main` branch, which will trigger automated generation and deployment of the release (Codox docs to github.io, JARs to Clojars, etc.).
+
+## License
+
+Copyright © 2021 Peter Monks
+
+Distributed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+SPDX-License-Identifier: [Apache-2.0](https://spdx.org/licenses/Apache-2.0)
---
+++ src/tools_pom/tasks.clj
@@ -0,0 +1,102 @@
+;
+; Copyright © 2021 Peter Monks
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+;
+; SPDX-License-Identifier: Apache-2.0
+;
+
+(ns tools-pom.tasks
+  "Clojure tools.deps tasks related to comprehensive pom.xml files.
+
+  The following build task functions are provided, with the
+  specified required and optional hash map options:
+
+  pom            -- opt: :lib a symbol identifying your project e.g. 'com.github.pmonks/tools-pom
+                         :version a string containing the version of your project e.g. \"1.0.0-SNAPSHOT\"
+                         :pom-file the name of the file to write to (defaults to \"./pom.xml\")
+                         :write-pom a flag indicating whether to invoke \"clj -Spom\" after generating the basic pom (i.e. adding dependencies and repositories from your deps.edn file) (defaults to false)
+                         :validate-pom a flag indicating whether to validate the generated pom.xml file after it's been constructed (defaults to false)
+                         :pom a map containing other POM elements (see https://maven.apache.org/pom.html for details).
+
+  All of the above build tasks return the opts hash map they were passed
+  (unlike some of the functions in clojure.tools.build.api)."
+  (:require [clojure.java.io         :as io]
+            [clojure.data.xml        :as xml]
+            [tools-convenience.api   :as tc]
+            [camel-snake-kebab.core  :as csk]
+            [clj-xml-validation.core :as xmlv]))
+
+; As of v1.10 this should be in core...
+(defmethod print-method java.time.Instant [^java.time.Instant inst writer]
+  (print-method (java.util.Date/from inst) writer))
+
+(defn- pom-keyword
+  "Converts a regular Clojure keyword into a POM-4.0.0-namespaced keyword."
+  [kw]
+  (keyword "xmlns.http%3A%2F%2Fmaven.apache.org%2FPOM%2F4.0.0" (csk/->camelCase (name kw))))
+
+(defn- build-pom
+  "Converts the given 'element' into a POM structure in clojure.data.xml compatible EDN format."
+  [elem]
+  (cond
+    (map-entry?  elem)             [(pom-keyword (key elem)) (build-pom (val elem))]
+    (map?        elem)             (map build-pom elem)
+    (sequential? elem)             (mapcat #(hash-map (pom-keyword (first elem)) (build-pom %)) (rest elem))
+    (= java.util.Date (type elem)) (str (.toInstant ^java.util.Date elem))
+    :else                          (str elem)))
+
+(defn pom
+  "Writes out a pom file. opts includes:
+
+  :lib          -- opt: a symbol identifying your project e.g. 'com.github.yourusername/yourproject
+  :version      -- opt: a string containing the version of your project e.g. \"1.0.0-SNAPSHOT\"
+  :pom-file     -- opt: the name of the file to write to (defaults to \"./pom.xml\")
+  :write-pom    -- opt: a flag indicating whether to invoke \"clojure -Spom\" after generating the basic pom (i.e. adding dependencies and repositories from your deps.edn file) (defaults to false)
+  :validate-pom -- opt: a flag indicating whether to validate the generated pom.xml file after it's been constructed (defaults to false)
+  :pom          -- opt: a map containing any other POM elements (see https://maven.apache.org/pom.html for details), using standard Clojure :keyword keys
+
+  Here is an example :pom map that will generate a valid pom.xml:
+
+  :pom {:description      \"Description of your project e.g. your project's short description on GitHub.\"
+        :url              \"https://github.com/yourusername/yourproject\"
+        :licenses         [:license   {:name \"Apache License 2.0\" :url \"http://www.apache.org/licenses/LICENSE-2.0.html\"}] ; Note first element is a tag
+        :developers       [:developer {:id \"yourusername\" :name \"yourname\" :email \"youremail@emailservice.com\"}]         ; And here
+        :scm              {:url                  \"https://github.com/yourusername/yourproject\"
+                           :connection           \"scm:git:git://github.com/yourusername/yourproject.git\"
+                           :developer-connection \"scm:git:ssh://git@github.com/yourusername/yourproject.git\"}
+        :issue-management {:system \"github\" :url \"https://github.com/yourusername/yourproject/issues\"}}))"
+  [opts]
+  (tc/ensure-command "clojure")
+  (println "ℹ️ Generating comprehensive pom.xml...")
+  (let [pom-file (get opts :pom-file "./pom.xml")
+        pom-in   (merge (when (:lib     opts) {:group-id (namespace (:lib opts)) :artifact-id (name (:lib opts)) :name (name (:lib opts))})
+                        (when (:version opts) {:version (:version opts)})
+                        (:pom opts))   ; Merge user-supplied values last, so that they always take precedence
+        pom-out  [(pom-keyword :project) {:xmlns                         "http://maven.apache.org/POM/4.0.0"
+                                          (keyword "xmlns:xsi")          "http://www.w3.org/2001/XMLSchema-instance"
+                                          (keyword "xsi:schemaLocation") "http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"}
+                   (concat [[(pom-keyword :model-version) "4.0.0"]]
+                            (build-pom pom-in))]
+        pom-xml  (xml/sexp-as-element pom-out)]
+    (with-open [pom-writer (io/writer pom-file)]
+      (xml/emit pom-xml pom-writer :encoding "UTF8"))
+    (when (:write-pom opts)
+      (tc/exec "clojure -Srepro -Spom"))   ; tools.build/write-pom is nowhere as useful as clojure -Spom but the latter doesn't have an API so we just fork it instead..
+    (when (:validate-pom opts)
+      (let [is-pom?           (xmlv/create-validation-fn (io/reader "https://maven.apache.org/xsd/maven-4.0.0.xsd"))
+            validation-result (is-pom? (slurp pom-file))]
+        (when-not (xmlv/valid? validation-result)
+          (println "⚠️ Generated pom.xml is not valid; check your input data. Errors:")
+          (doall (map #(println "  Line" (get % :line-number "?") "column" (str (get % :column-number "?") ":") (:message %))  (xmlv/errors validation-result)))))))
+  opts)
